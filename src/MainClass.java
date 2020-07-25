031dcc1c0b7d932129f899463e4353f0a5e4d5f2
Mikalai <ionick@mail.ru>
2020-07-25 17:06:53 +0300

    Create getClassNumber and testGetClassNumber

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,13 +2,12 @@ import org.junit.Test;
 
 public class MainClass
 {
+    int class_number = 20;
+
   @Test
-      public void getLocalNumber()
-
-    {
-        int number = 14;
-        System.out.println(number);
-    }
-
+    public void getClassNumber()
+  {
+      System.out.println(this.class_number);
+  }
 
 }
---
+++ src/MainClassTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class MainClassTest extends MainClass
+{
+    @Test
+
+    public void  testGetClassNumber()
+    {
+        if (this.class_number > 45) {
+            System.out.println("Value more then 45");
+        } else {
+            System.out.println("Value is 45 or less then 45");
+        }
+    }
+}
